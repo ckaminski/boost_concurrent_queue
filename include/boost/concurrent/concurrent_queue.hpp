d1191b0f5214de79c565f33fa38ed7cdd6d0f343
Chris Kaminski <ck1@darthcoder.com>
2017-07-21 18:26:03 -0400

    Initial checkin

---
+++ include/boost/concurrent/concurrent_queue.hpp
@@ -0,0 +1,12 @@
+#include <boost/thread.hpp> 
+#include <vector> 
+
+template <class Type> 
+class concurrent_queue { 
+    std::vector<Type> values;
+
+  public: 
+
+    
+
+}; 
---
+++ test/test1.cpp
@@ -0,0 +1,15 @@
+#define CATCH_CONFIG_MAIN 
+#include <catch.hpp>
+#include <boost/concurrent/concurrent_queue.hpp>
+
+unsigned int Factorial( unsigned int number ) {
+    return number <= 1 ? number : Factorial(number-1)*number;
+}
+
+
+TEST_CASE( "Test 1", "[base]" ) {
+    REQUIRE( Factorial(1) == 1 );
+    REQUIRE( Factorial(2) == 2 );
+    REQUIRE( Factorial(3) == 6 );
+    REQUIRE( Factorial(10) == 3628800 );
+}
